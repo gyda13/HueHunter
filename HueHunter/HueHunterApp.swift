5e406bcded968a7d69a82269e8b498edf4917de9
gyda13 <gyda2100@gmail.com>
2023-01-08 15:02:59 +0300

    Initial Commit

---
+++ HueHunter/HueHunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HueHunterApp.swift
+//  HueHunter
+//
+//  Created by gyda almohaimeed on 15/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct HueHunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

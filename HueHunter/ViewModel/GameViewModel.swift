8c985bed164b08c7796e063509338e5f71252533
gyda13 <gyda2100@gmail.com>
2023-04-28 15:15:47 +0300

    adding sound effect

--- HueHunter/ViewModel/GameViewModel.swift
+++ HueHunter/ViewModel/GameViewModel.swift
@@ -6,8 +6,51 @@
 //
 
 import SwiftUI
+import AVFoundation
+import AVKit
 
-class GameViewModel: ObservableObject {
+class SoundManager {
+    
+    static let instance = SoundManager()
+    
+    var player: AVAudioPlayer?
+    
+    func playSoundMatch() {
+        
+        guard let url = Bundle.main.url(forResource: "match", withExtension: ".wav") else { return }
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch {
+            print("error play sound")
+        }
+    }
+    func playSoundFail() {
+        
+        guard let url = Bundle.main.url(forResource: "fail", withExtension: ".mp3") else { return }
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch {
+            print("error play sound")
+        }
+    }
+    func playSoundWin() {
+        
+        guard let url = Bundle.main.url(forResource: "win", withExtension: ".mp3") else { return }
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch {
+            print("error play sound")
+        }
+    }
+}
+
+class GameViewModel: Identifiable, ObservableObject {
     // MARK: - Gesture Properties
     @Published var currentToy: Object?
     @Published var currentPosition = initialPosition
@@ -15,19 +58,36 @@ class GameViewModel: ObservableObject {
     @Published var draggableToyOpacity: CGFloat = 1.0
     @Published var isGameOver = false
     private(set) var attempts = 0
+    @Published var object: Object?
     
+    @Published var index =  3
     
     // MARK: - Coordinates
     private static let initialPosition = CGPoint(
-        x: UIScreen.main.bounds.midX,
-        y: UIScreen.main.bounds.maxY - 135
+        x: UIScreen.main.bounds.midX ,
+        y: UIScreen.main.bounds.maxY - 170
     )
     private var frames: [Int: CGRect] = [:]
     
     // MARK: - Objects in the screen
-    private var toys = Array(Object.all.shuffled().prefix(upTo: 9))
-    var toyContainers = Object.all.shuffled()
     
+    
+    private var animals = Array(Object.animals.shuffled().prefix(upTo: 9))
+    var animalsContainers = Object.animals.shuffled()
+    
+    
+    private var sports = Array(Object.sports.shuffled().prefix(upTo: 9))
+    var sportsContainers = Object.sports.shuffled()
+    
+    private var FruitsandVegetables = Array(Object.animals.shuffled().prefix(upTo: 9))
+    var FruitsandVegetablesContainers = Object.FruitsandVegetables.shuffled()
+    
+    
+    private var nature = Array(Object.sports.shuffled().prefix(upTo: 9))
+    var natureContainers = Object.nature.shuffled()
+    
+    
+
     // MARK: - Game lifecycle
     func confirmWhereToyWasDropped() {
         defer { highlightedId = nil }
@@ -41,7 +101,9 @@ class GameViewModel: ObservableObject {
             setCurrentPositionToHighlightedContainer(WithId: highlightedId)
             generateNextRound()
             attempts += 1
+            SoundManager.instance.playSoundMatch()
         } else {
+            SoundManager.instance.playSoundFail()
             resetPosition()
         }
         
@@ -69,19 +131,25 @@ class GameViewModel: ObservableObject {
         
         if currentToy == nil {
             gameOver()
-          
         } else {
             prepareObjects()
         }
     }
     
     func setNextToy() {
-        currentToy = toys.popLast()
+            currentToy = animals.popLast()
+        
+            currentToy = sports.popLast()
+       
+            currentToy = FruitsandVegetables.popLast()
+      
+            currentToy = nature.popLast()
+       
     }
     
     func gameOver() {
         isGameOver = true
-     
+       
     }
     
     func prepareObjects() {
@@ -90,8 +158,17 @@ class GameViewModel: ObservableObject {
     }
     
     func shuffleToyContainersWithAnimation() {
-        withAnimation {
-            toyContainers.shuffle()
+
+            withAnimation {
+                animalsContainers.shuffle()
+     
+                sportsContainers.shuffle()
+      
+           
+                FruitsandVegetablesContainers.shuffle()
+      
+                natureContainers.shuffle()
+            
         }
     }
     
@@ -109,9 +186,31 @@ class GameViewModel: ObservableObject {
     }
     
     func generateNewGame() {
-        toys = Array(Object.all.shuffled().prefix(upTo: 9))
-        attempts = 0
-        generateNextRound()
+        if(index == 0) {
+           animals = Array(Object.animals.shuffled().prefix(upTo: 9))
+            attempts = 0
+            generateNextRound()
+        }
+        if(index == 1) {
+            sports = Array(Object.nature.shuffled().prefix(upTo: 9))
+            attempts = 0
+            generateNextRound()
+        }
+        
+        if(index == 2) {
+            animals = Array(Object.FruitsandVegetables.shuffled().prefix(upTo: 9))
+            attempts = 0
+            generateNextRound()
+        }
+        if(index == 3) {
+           sports = Array(Object.sports.shuffled().prefix(upTo: 9))
+            attempts = 0
+            generateNextRound()
+        }
+        
+       
+        
+      
     }
     
     // MARK: - Updates in the screen
